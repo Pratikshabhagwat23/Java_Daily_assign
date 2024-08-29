d8c477e68fe107901af4a889447b39c5ae48620b
Pratiksha <bhagwatpratiksha23@gmail>
2024-08-29 23:49:13 +0530

    programs added

---
+++ Day_4/program1.java
@@ -0,0 +1,13 @@
+class Codex{
+
+	static public void main(String[]args){
+	char ch = 'A';
+	for(int i = 0; i<9; i++){
+	if(i % 2 == 0){
+		System.out.print((char) (ch+i)+ " ");
+			}
+		}
+
+	}
+
+}
