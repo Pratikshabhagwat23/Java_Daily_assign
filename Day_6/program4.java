aadbb11235ec42ec9e97ca6bca157181c348aff8
Pratiksha <bhagwatpratiksha23@gmail>
2024-09-04 09:59:46 +0530

    assign6

---
+++ Day_6/program4.java
@@ -0,0 +1,15 @@
+class Codex{
+
+	static public void main(String[]args){
+	char ch= 'A';
+	for(int i=1; i<=3; i++){
+		for(int j=1; j<=4; j++){
+			System.out.print(ch+" ");
+			ch++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
