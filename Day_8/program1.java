237e3032f53d776a29ddfd01f7b56bd6ff7c0a25
Pratiksha <bhagwatpratiksha23@gmail>
2024-09-06 09:18:07 +0530

    programs added

---
+++ Day_8/program1.java
@@ -0,0 +1,15 @@
+class Demo{
+
+	static public void main(String[]args){
+	int a = 1;
+	for(int i=1; i<=4; i++){
+		for(int j=1; j<=i; j++){
+			System.out.print(a*a+"  ");
+			a++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
